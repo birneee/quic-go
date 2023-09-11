626d7fd5a1a4999c348e01cb0495063a5cc67248
Benedikt Spies <benedikt.spies@gmail.com>
2023-09-11 11:50:12 +0200

    expose GSO capability through connection state

--- connection.go
+++ connection.go
@@ -671,6 +671,7 @@ func (s *connection) ConnectionState() ConnectionState {
 	cs := s.cryptoStreamHandler.ConnectionState()
 	s.connState.TLS = cs.ConnectionState
 	s.connState.Used0RTT = cs.Used0RTT
+	s.connState.GSO = s.conn.capabilities().GSO
 	return s.connState
 }
 
